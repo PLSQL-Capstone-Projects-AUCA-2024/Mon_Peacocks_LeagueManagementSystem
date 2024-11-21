c800718746f5a40ce5a6810b6db5c07c10350ba4
DeNoella <116192621+DeNoella@users.noreply.github.com>
2024-11-21 22:46:48 +0200

    Testing and Verifying the created tables

---
+++ data.sql
@@ -0,0 +1,8 @@
+TESTING AND VERIFYING THE CREATED TABLES
+
+DECLARE
+    stadium_count NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO stadium_count FROM Stadiums;
+    DBMS_OUTPUT.PUT_LINE('Total stadiums: ' || stadium_count);
+END;
