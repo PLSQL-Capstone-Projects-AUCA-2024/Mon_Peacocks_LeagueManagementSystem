9f5cb63d6245ede621c9ed23377bef4724c4db5e
KEWACO <60927778+Kewaco0@users.noreply.github.com>
2024-11-21 23:36:48 +0200

    Testqueries.sql

---
+++ data.sql
@@ -0,0 +1,7 @@
+
+Test queries on matches with composite index
+
+EXPLAIN PLAN FOR SELECT * FROM Matches WHERE home_team_id = 1 AND away_team_id = 2;
+
+
+Prepare deployment scripts and ensure all data is consistent across environments.
