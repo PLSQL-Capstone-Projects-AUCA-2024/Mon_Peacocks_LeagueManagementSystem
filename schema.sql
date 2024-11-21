03fc9476c18a431e9d59207d446a9be9bb8e5fee
edenlisk <nkumbuyenock@gmail.com>
2024-11-21 22:15:25 +0200

    Added template files

--- schema.sql
+++
@@ -1,7 +0,0 @@
--- CREATING DATABASE
-CREATE PLUGGABLE DATABASE mon_peacock_leagueManagementSystem
-ADMIN USER mon_peacock IDENTIFIED BY peacock
-FILE_NAME_CONVERT = ('E:\oracle\oradata\ORCL\pdbseed', 'E:\plsql\pdbs\peacock');
-
-
-GRANT DBA TO mon_peacock;
