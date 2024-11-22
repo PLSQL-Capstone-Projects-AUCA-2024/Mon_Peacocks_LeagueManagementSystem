585a6adebea301879efa1a786f882e7125f437e6
Icyeza-Nelly <icyezanelly2021@gmail.com>
2024-11-22 02:09:01 +0200

    Update data.sql
    -Verify that all league seasons have valid champion team IDs
    -Ensure no player has overlapping team memberships

---
+++ data.sql
@@ -0,0 +1,11 @@
+Verify that all league seasons have valid champion team IDs
+
+SELECT * FROM LeagueSeasons WHERE champion_team_id NOT IN (SELECT team_id FROM Teams);
+
+Ensure no player has overlapping team memberships
+
+SELECT player_id 
+FROM TeamPlayers 
+GROUP BY player_id 
+HAVING COUNT(*) > 1 AND MIN(leave_date) > MAX(join_date);
+
